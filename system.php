48d7da9061bcc10fe7bd34f874e5b07ff73e8eae
malikumer <lablnet01@gmail.com>
2018-05-30 21:03:48 +0500

    update system file

--- system.php
+++ system.php
@@ -3,17 +3,10 @@ require_once __DIR__ . "/vendor/autoload.php";
 $File = new Softhub99\Zest_Framework\Files\Files;
 $File->systemDirs();
 $session = new Softhub99\Zest_Framework\Session\Session;
-/**
- * Error and Exception handling
- */
 error_reporting(E_ALL);
 set_error_handler('Softhub99\Zest_Framework\Expection\Expection::errorHandler');
 set_exception_handler('Softhub99\Zest_Framework\Expection\Expection::exceptionHandler');
-
-/**
- * Routing
- */
-
 $router = new Softhub99\Zest_Framework\Router\Router();
+$server = new Softhub99\Zest_Framework\Common\Server();
+$server->get();
 require_once route()->routes.'routes.php';
-
