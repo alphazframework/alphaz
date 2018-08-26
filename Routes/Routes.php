a9946705f6f1547dd56f6f063ff9aeb14bd15d38
Malik Umer Farooq <Lablnet01@gmail.com>
2018-08-26 07:02:26 +0000

    Apply fixes from StyleCI

--- Routes/Routes.php
+++ Routes/Routes.php
@@ -1,11 +1,10 @@
 <?php
 
 // Add the Routes
-$router->get('',"Home@index");
-
+$router->get('', 'Home@index');
 
 //for test middleware
-$router->get('middlewaretest',"Home@middlewaretest");
+$router->get('middlewaretest', 'Home@middlewaretest');
 
 //Test using closure
 $router->get('user/{id:[0-9]}', function ($args) {
