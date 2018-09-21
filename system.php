346c9ba3a8699f3d5ae7eb434a1baecbce8dccd4
Lablnet <lablnet01@gmail.com>
2018-09-20 22:06:09 -0700

    update system.php

--- system.php
+++ system.php
@@ -1,12 +1,11 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
-$File = new Zest\Files\Files();
-$File->systemDirs();
-$session = new Zest\Session\Session();
+(new Zest\Files\Files())->systemDirs();
+(new Zest\Session\Session());
 error_reporting(E_ALL);
 set_error_handler('Zest\Expection\Expection::errorHandler');
 set_exception_handler('Zest\Expection\Expection::exceptionHandler');
-$app = (new Zest\Router\App())->run();
-$maintenance = (new Zest\Common\Maintenance())->run();
-$server = (new Zest\Common\Server())->get();
+(new Zest\Router\App())->run();
+(new Zest\Common\Maintenance())->run();
+(new Zest\Common\Server())->get();
