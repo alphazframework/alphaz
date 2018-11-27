7304433fb8cf632d4638add42af07c7b3524914f
Lablnet <lablnet01@gmail.com>
2018-11-26 19:48:15 -0800

    update local added defaults msgs

--- App/Local/en.php
+++ App/Local/en.php
@@ -4,4 +4,17 @@ return  [
     'title:home'     => 'Zest Framework',
     'framework:home' => 'Zest',
     'app:home'       => 'Start building your app',
+	
+    //Validations
+    'string:validation'         => 'The :field much be type of string like, mango',
+    'required:validation'       => 'The :field is required',
+    'int:validation'            => 'The :field must be int like, 20, 200',
+    'float:validation'          => 'The :name field must be of type float e.g. 20.0',
+    'email:validation'          => 'Email address is not valid',
+    'ip:validation'             => 'Ip address is not valid',
+    'ipv6:validation'           => 'The value must be a valid IPv6 address',
+    'alpha:validation'          => 'The value must only contain characters of the alphabet',
+    'subnet:validation'         => 'The value must be a valid IPv4 subnet',
+    'validate:validation'       => 'Json is invilide',
+    'unique:validation'         => 'The :field value already exists, try another',    	
 ];
