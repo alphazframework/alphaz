bd67a6eac622b1ca78a0511d9ccbc5af7d338143
Lablnet <lablnet01@gmail.com>
2019-01-16 07:16:33 +0500

    Update Home controller based on 3.0.0

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -2,11 +2,6 @@
 
 namespace App\Controllers;
 
-//for using Middleware
-use Zest\Middleware\Middleware;
-//for using View
-use Zest\View\View;
-
 class Home extends \Zest\Controller\Controller
 {
     /**
@@ -16,13 +11,6 @@ class Home extends \Zest\Controller\Controller
      */
     public function index()
     {
-        echo View::view('Home/index');
-    }
-
-    public function middlewaretest()
-    {
-        Middleware::run('before', 'ExampleMiddleware', $params = null);
-        echo ' Middleware Test ';
-        Middleware::run('after', 'ExampleMiddleware', $params = null);
+        view('Home/index');
     }
 }
