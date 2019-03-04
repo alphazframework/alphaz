d5c9c187b81ebb1ea0e842ad488df6d6f9795306
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-03-04 07:09:38 +0500

    Update system.php

--- system.php
+++ system.php
@@ -4,5 +4,5 @@ require_once __DIR__.'/vendor/autoload.php';
 $expection = new \Zest\Expection\Expection();
 (new Zest\Files\Files())->systemDirs();
 (new Zest\Session\Session());
-(new Zest\Router\App())->run();
+(new Zest\Bootstrap())->boot();
 (new Zest\Common\Maintenance())->run();
