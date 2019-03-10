0d04f36c0b56127d71ac95fc63f95066475efa6c
Lablnet <lablnet01@gmail.com>
2019-03-10 13:44:17 +0500

    update welcome component

--- App/Components/welcome/routes.php
+++ App/Components/welcome/routes.php
@@ -4,8 +4,8 @@
 //namespace required to define your component you can add many routes in one component as well
 $namespace = "App\Components\welcome\Controllers";
 
-// Its create the url localhost/blog/helloworld'
+// Its create the url localhost/welcome'
 $com->add('welcome', ['controller' => 'Home', 'action' => 'index', 'namespace'=>$namespace]);
 
 //Route with middleware
-$com->get('welcome/middleware', "Home@index@$namespace", "App\Components\hello\Middleware\@Hello");
+$com->get('welcome/middleware', "Home@index@$namespace", "App\Components\welcome\Middleware\@Welcome");
