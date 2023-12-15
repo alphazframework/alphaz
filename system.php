d8c97d80e165d50b8c83e906f4f2e529cfab4ee6
Muhammad Umer Farooq <umer@lablnet.com>
2023-12-15 09:12:30 +0500

    Apply fixes from StyleCI (#50)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- system.php
+++ system.php
@@ -5,6 +5,6 @@ define('__ZEST__ROOT__', __DIR__);
 require_once __DIR__.'/vendor/autoload.php';
 $expection = new \Zest\Exception\Exception();
 //(new Zest\Files\Files())->systemDirs();
-(new Zest\Session\Session());
+new Zest\Session\Session();
 (new Zest\Bootstrap())->boot();
 (new Zest\Common\Maintenance())->run();
